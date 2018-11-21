eeaf2910c9c612b0dad6504dcde5be466a32f239
Matthew <mbdevaney@gmail.com>
2018-11-21 15:01:12 -0600

    alphavanatge script now reads from db

--- stockindex_app/models.py
+++ stockindex_app/models.py
@@ -17,7 +17,7 @@ class Stock(models.Model):
     high_price_52_weeks = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     low_price_52_weeks = models.DecimalField(decimal_places=2, max_digits=10, null=True)
     market_cap = models.DecimalField(decimal_places=2, max_digits=20, null=True)
-    logo = models.ImageField(upload_to='')
+    logo = models.ImageField(upload_to='', null=True)
     inactive = models.BooleanField(default=False)
 
     def __str__(self):
@@ -26,7 +26,7 @@ class Stock(models.Model):
 
 class Observations(models.Model):
     """Daily statistics about how a stock is trading"""
-    stock_id = models.ForeignKey('Stock', on_delete=models.CASCADE)
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE)
     observation_date = models.DateTimeField()
     volume = models.IntegerField(null=True)
     open_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
@@ -52,5 +52,5 @@ class Index(models.Model):
 
 class StocksIndexed(models.Model):
     """Describes which stocks belong to an index"""
-    index_id = models.ForeignKey('Index', on_delete=models.CASCADE)
-    stock_id = models.ForeignKey('Stock', on_delete=models.CASCADE)
+    index = models.ForeignKey('Index', on_delete=models.CASCADE)
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE)
