215fc6b8d2e408ae60cc340701966c33c5b47126
mdevaney <mbdevaney@gmail.com>
2018-12-27 20:17:56 -0600

    added comments and documentation

--- scripts/update.py
+++ scripts/update.py
@@ -1,24 +1,29 @@
+# custom packages
 from AlphaVantage import AlphaVantage
-import datetime
 from SqlQuery import SqlConnection
-from sqlalchemy import select, insert, func, update
 from TmxMoney import TmxMoney
-import pandas as pd
 
+# python packages
+import datetime
+import pandas as pd
+from sqlalchemy import select, insert, func, update
+
+# init logging
 import logging
 logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Observation:
-
-    columns_list = ['marketsymbol', 'observation_date', 'volume', 'open_price',
-                    'high_price', 'low_price', 'close_price']
+    """Daily statistics about how a stock is trading"""
 
     def __init__(self, obs_records):
+        columns_list = ['marketsymbol', 'observation_date', 'volume', 'open_price',
+                        'high_price', 'low_price', 'close_price']
         self.df = pd.DataFrame.from_records(obs_records, columns=self.columns_list)
         self.df['observation_date'] = pd.to_datetime(self.df['observation_date'], format='%Y-%m-%d %H:%M:%S')
 
     def get_stock_fk(self):
+        """get the foreign keys for each symbol in the observations DataFrame"""
         tbl = 'stockindex_app_stock'
         stock = SqlConnection(tbl)
         stmt = select([stock.table.c.marketsymbol, stock.table.c.id]).where(stock.table.c.inactive == 0)
@@ -29,6 +34,7 @@ class Observation:
         self.df.rename(columns={'id': 'stock_id'}, inplace=True)
 
     def write(self):
+        """insert new observations into the SQL database"""
         tbl = 'stockindex_app_observations'
         observation = SqlConnection(tbl)
         stmt = insert(observation.table)
@@ -36,7 +42,7 @@ class Observation:
 
 
 class Stock:
-
+    """A single publicly traded stock"""
     def __init__(self):
         tbl = 'stockindex_app_stock'
         stock = SqlConnection(tbl)
@@ -44,6 +50,7 @@ class Stock:
         self.df = stock.select_query(stmt)
 
     def update_price(self, df_obs):
+        """update the current, prior, high, low and change price of a stock"""
         self.df['prior_close_price'] = self.df['current_price']
         self.df.drop(['current_price', 'high_price', 'low_price'], axis=1, inplace=True)
         df_obs = df_obs[['stock_id', 'close_price', 'high_price', 'low_price']]
@@ -54,6 +61,7 @@ class Stock:
         self.df.drop('stock_id', axis=1, inplace=True)
 
     def update_52_week_highlow(self):
+        """compute the 52 week high/low price of a stock"""
         tbl = 'stockindex_app_observations'
         observation = SqlConnection(tbl)
         highest_price = func.max(observation.table.c.high_price).label('high_price_52_weeks')
@@ -68,6 +76,7 @@ class Stock:
         self.df.drop('stock_id', axis=1, inplace=True)
 
     def calculate_mktcap(self):
+        """find the market capitalization of a stock: shares outstanding * stock price"""
         shares_out_list = [x.replace('-', '.') for x in self.df['symbol'].tolist()]
         tmx = TmxMoney(shares_out_list)
         df_shares_out = tmx.get_shares_outstanding()
@@ -81,6 +90,7 @@ class Stock:
             (pd.to_numeric(self.df['low_price']) * pd.to_numeric(self.df['shares_outstanding'])).astype('int64')
 
     def write(self):
+        """update the stock information in the SQL database"""
         tbl = 'stockindex_app_stock'
         stock = SqlConnection(tbl)
         values_list = [x for x in self.df.T.to_dict().values()]
@@ -105,6 +115,7 @@ class Stock:
 
 
 class IndexObservations:
+    """Daily statistics about how an index is trading"""
 
     def __init__(self):
         tbl = 'stockindex_app_index'
@@ -113,6 +124,7 @@ class IndexObservations:
         self.df = index.select_query(stmt)
 
     def get_observation(self):
+        """update the current, prior, high, low and change value of the index"""
         tbl = 'stockindex_app_stocksindexed'
         stocksindexed = SqlConnection(tbl)
         stmt = select([stocksindexed.table])
@@ -149,6 +161,7 @@ class IndexObservations:
         self.df = self.df[['observation_date', 'open_value', 'high_value', 'low_value', 'close_value', 'index_id']]
 
     def write(self):
+        """insert index observations into the SQL database"""
         tbl = 'stockindex_app_indexobservations'
         observation = SqlConnection(tbl)
         stmt = insert(observation.table)
@@ -156,6 +169,7 @@ class IndexObservations:
 
 
 class Index:
+    """An indicator of stock market performance based on information about selected stocks"""
 
     def __init__(self):
         tbl = 'stockindex_app_index'
@@ -164,6 +178,7 @@ class Index:
         self.df = index.select_query(stmt)
 
     def update_price(self, df_index_obs):
+        """update the current, prior, high, low and change value of the index"""
         self.df['prior_close_value'] = self.df['current_value']
         self.df.drop(['current_value', 'high_value', 'low_value'], axis=1, inplace=True)
         self.df = pd.merge(self.df, df_index_obs, how='inner', left_on='id', right_on='index_id')
@@ -173,6 +188,7 @@ class Index:
         self.df.drop(['open_value'], axis=1, inplace=True)
 
     def update_52_week_highlow(self):
+        """compute the 52 week high/low value of an index"""
         tbl = 'stockindex_app_indexobservations'
         index_obs = SqlConnection(tbl)
         highest_value = func.max(index_obs.table.c.high_value).label('high_value_52_weeks')
@@ -187,6 +203,7 @@ class Index:
                            'high_value_52_weeks', 'low_value_52_weeks']]
 
     def write(self):
+        """update the index information in the SQL database"""
         tbl = 'stockindex_app_index'
         index = SqlConnection(tbl)
         values_list = [x for x in self.df.T.to_dict().values()]
@@ -207,6 +224,7 @@ class Index:
 
 
 def get_mktsymbol_list():
+    """get a list of symbols to lookup on AlphaVantage"""
     tbl = 'stockindex_app_stock'
     stock = SqlConnection(tbl)
     stmt = select([stock.table.c.marketsymbol]).where(stock.table.c.inactive == 0)
@@ -216,21 +234,6 @@ def get_mktsymbol_list():
 
 
 if __name__ == "__main__":
-    logging.debug('Stock updates loaded into database')
-    io = IndexObservations()
-    logging.debug('Index observations object created')
-    io.get_observation()
-    logging.debug('Index observations object updated')
-    io.write()
-    logging.debug('Index observations written to database')
-    i = Index()
-    logging.debug('Index object created')
-    i.update_price(io.df)
-    logging.debug('Index prices updated')
-    i.update_52_week_highlow()
-    i.write()
-
-    """ 
     logging.debug('Start WINDEX update script')
     a = AlphaVantage(get_mktsymbol_list(), 'prior', 'VWXATT8K62KW1GZH')
     logging.debug('AlphaVantage API object created')
@@ -248,33 +251,16 @@ if __name__ == "__main__":
     s.calculate_mktcap()
     logging.debug('Calculated market capitalization')
     s.write()
-    
-    test_records = [('TSX:BUI', '2018-12-03 00:00:00', 0, 3.6900, 3.6900, 3.6900, 3.6900),
-                    ('TSX:BYD-UN', '2018-12-03 00:00:00', 83073, 115.9700, 121.2100, 114.1700, 118.7800),
-                    ('TSX:GWO', '2018-12-03 00:00:00', 725300, 30.5500, 30.5900, 29.8400, 30.0000),
-                    ('TSX:IGM', '2018-12-03 00:00:00', 328800, 34.2600, 34.8400, 34.1900, 34.8400),
-                    ('TSX:PBL', '2018-12-03 00:00:00', 6200, 22.1000, 22.3700, 22.0100, 22.3700),
-                    ('TSX:NFI', '2018-12-03 00:00:00', 448300, 38.2100, 38.4200, 37.1800, 37.3600),
-                    ('TSX:NWC', '2018-12-03 00:00:00', 76200, 29.2900, 29.4400, 28.8100, 29.3000),
-                    ('TSX:AFN', '2018-12-03 00:00:00', 18400, 54.4400, 54.4400, 53.3400, 53.6300),
-                    ('TSX:EIF', '2018-12-03 00:00:00', 67300, 31.3000, 31.4800, 30.4500, 30.9900)]
-
-    test_records_update = [(1, 'BUI', 'Buhler Industries',   3.69,  0, 0, 9.22500000e+07, 'TSX', 'TSX:BUI', '',
-                        3.69,  3.69,  3.69, 3.94, 3.53, 25000000),
-                     (2, 'BYD-UN', 'The Boyd Group', 111.1,  7.68, 0, 2.33681536e+09, 'TSX', 'TSX:BYD-UN', '',
-                      118.78, 115.97, 121.21, 133, 102.59,  19673475),
-                     (3, 'GWO', 'Great West Life Company',  27.87,  2.13, 0, 2.96515036e+10, 'TSX', 'TSX:GWO', '',
-                      30,  30.55,  30.59, 33.1,  28.37, 988383452),
-                     (4, 'IGM', 'IGM Financial',  22.37, 12.47, 0, 8.39154568e+09, 'TSX', 'TSX:IGM', '',
-                      34.84,  34.26,  34.84, 39.95, 31.54, 240859520),
-                     (5, 'PBL', 'Pollard Banknote',  19.53,  2.84, 0, 5.73245969e+08, 'TSX', 'TSX:PBL', '',
-                      22.37,  22.1,  22.37, 27.75,  19.91,  25625658),
-                     (6, 'NFI', 'New Flyer Industries',  34.31,  3.05, 0, 2.32951738e+09, 'TSX', 'TSX:NFI', '',
-                      37.36,  38.21,  38.42, 52.48,  32.95,  62353249),
-                     (7, 'NWC', 'Northwest Company',  28,  1.3, 0, 1.42685108e+09, 'TSX', 'TSX:NWC', '',
-                      29.3 ,  29.29,  29.44, 30.6 ,  27.03,  48697989),
-                     (8, 'EIF', 'Exchange Income Corp',  30.23,  0.76, 0, 9.72296840e+08, 'TSX', 'TSX:EIF', '',
-                      30.99,  31.3,  31.48, 35.34,  26.87,  31374535),
-                     (9, 'AFN', 'Ag Growth International',  50,  3.63, 0, 9.84849521e+08, 'TSX', 'TSX:AFN', '',
-                      53.63,  54.44,  54.44, 64.72,  47.84,  18363780)]
-"""
+    logging.debug('Stock updates loaded into database')
+    io = IndexObservations()
+    logging.debug('Index observations object created')
+    io.get_observation()
+    logging.debug('Index observations object updated')
+    io.write()
+    logging.debug('Index observations written to database')
+    i = Index()
+    logging.debug('Index object created')
+    i.update_price(io.df)
+    logging.debug('Index prices updated')
+    i.update_52_week_highlow()
+    i.write()
